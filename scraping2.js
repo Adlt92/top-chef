91b7cbaae33cd92296b5bfa277fdbf2ae4da2c49
AugustinDlt <augustininscription@gmail.com>
2018-03-04 23:31:40 +0100

    fix

--- scraping2.js
+++ scraping2.js
@@ -4,8 +4,7 @@ var cheerio = require('cheerio');
 var fs = require('fs');		
 
 
-function scrapID(callback){
-	console.log("scrap ID");
+function scrapID(nn, callback){
 	var json_file = [];
 	//extract the data from link_resto.json
 	fs.readFile('info_resto.json', function readFileCallback(err, data){
@@ -16,10 +15,11 @@ function scrapID(callback){
 		json_file = JSON.parse(data);
 		var nbr_resto = json_file.length-1;
 		//scraping each retaurant in France
+  
 		for (i = 0; i < nbr_resto; i++){
 			var urlsearch = "https://m.lafourchette.com/api/restaurant-prediction?name=" + json_file[i].name.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
 		  	var json = json_file[i];
-		  	search(urlsearch, json, function(new_json) {
+		  	search2(urlsearch, json, function(new_json) {
 				  json_file[i] = new_json;
 				  fs.writeFile('info_resto.json', JSON.stringify(json_file, null, 4), function(err){});
 				});
@@ -30,16 +30,16 @@ function scrapID(callback){
 	});
 }
 
-function search(url_s, json_fi, callback){
+function search2(url_s, json_fi, callback){
 	request({url: url_s, json : true}, function (error, response, body){
 		if(body != null){
 			try{
-				if(body){
+				if(body.length > 0){
 					body.forEach(function(element){
 						if(element.address.postal_code == json_fi.localisation.zipcode){
 							json_fi.fourchette.id = element.id;
 						}
-					});
+			});
 				}
 			} catch(error){
 				console.log(error);
@@ -49,9 +49,8 @@ function search(url_s, json_fi, callback){
 	  });
 }
 
-function scrapOffer(){
-	console.log("scrap offer");
-	json_file = [];
+function scrapO(name_file_in, callback){
+	var json_file = [];
 	fs.readFile('info_resto.json', function readFileCallback(err, data){
 		if (err){
 			console.log(err);
@@ -68,6 +67,7 @@ function scrapOffer(){
 						fs.writeFile('info_resto.json', JSON.stringify(json_file, null, 4), function(err){});
 					});
 				}
+				callback();
 			}
 		}
 	});
@@ -93,10 +93,13 @@ function req(url_s, json_fi, callback){
 	  });
 }
 
+
 function scrapingFourchette(){
+	console.log("start la fourchette")
 	var name_file_in = 'info_resto.json';
-	scrapID( function() {});
+	scrapID(name_file_in, function() {
+		scrapO('info_resto.json', function(){});
+	});
 }
 
-setTimeout(scrapingFourchette, 3*60*1000);
-setTimeout(scrapOffer,7*60*1000);
+setTimeout(scrapingFourchette,3 * 60 * 1000);
